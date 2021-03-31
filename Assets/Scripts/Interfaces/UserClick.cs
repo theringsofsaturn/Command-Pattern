9ca4a74626390218508f5bb2c9b21193c9829984
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-31 22:05:47 +0200

    Assign cubes random colors

--- Assets/Scripts/Interfaces/UserClick.cs
+++ Assets/Scripts/Interfaces/UserClick.cs
@@ -13,6 +13,22 @@ public class UserClick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //left click
+        //cast a ray
+        //detect a cube
+        //assign a random color
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if(hitInfo.collider.tag == "Cube")
+                {
+                    hitInfo.collider.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+                }
+            }
+        }
     }
 }
